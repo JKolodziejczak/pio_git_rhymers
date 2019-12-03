06e1a7e21dcb2718868bbf11925114b8e9c1fcb4
JKolodziejczak <eminemka099@gmail.com>
2019-12-03 13:57:12 +0100

    poprawione formatowanie

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,18 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;//poprawione
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo()) //poprawione
+            totalRejected++;
+        else //poprawione
+            super.countIn(in); //poprawione
+    }
 }
+
+//kombinacja tych klawiszy przechodzi odpowiednio do poprzedniego lub następnego słowa. W intellij odpowiednikiem alt + strzałka jest ctrl + strzałka.
