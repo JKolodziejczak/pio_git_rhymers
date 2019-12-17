b49b7a4b5202a34f421377729a828fbfba926218
Jarosław Pietrzak <215853@edu.p.lodz.pl>
2019-12-17 13:53:52 +0100

    Stałe i annotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,21 +2,23 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
-
     public int getTotal() {
         return total;
     }
 
-    private int total = -1;
+    public static final int SIZE = 12;
+    private static final int RETURN_NUMBER = -1;
+    private final int[] NUMBERS = new int[SIZE];
+
+    public int total = RETURN_NUMBER;
 
     public void countIn(int in) {
         if (!isFull())
-            numbers[++total] = in;
+            NUMBERS[++total] = in;
     }
 
     boolean callCheck() {
-        return total == -1;
+        return total == RETURN_NUMBER;
     } //poprawione
 
     boolean isFull() {
@@ -25,14 +27,14 @@ public class DefaultCountingOutRhymer {
 
     int peekaboo() { //poprawione
         if (callCheck()) //poprawione
-            return -1; //poprawione
-        return numbers[total]; //poprawione
+            return RETURN_NUMBER; //poprawione
+        return NUMBERS[total]; //poprawione
     }
 
     public int countOut() { //poprawione
         if (callCheck()) //poprawione
-            return -1; //poprawione
-        return numbers[total--]; //poprawione
+            return RETURN_NUMBER; //poprawione
+        return NUMBERS[total--]; //poprawione
     }
 
 }
