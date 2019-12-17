333c318b562cd9e4baea33fdf974601dc6c152e6
JKolodziejczak <eminemka099@gmail.com>
2019-12-17 14:16:48 +0100

    1.5 podpunkt 1 i 3

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -8,26 +8,30 @@ class RhymersDemo {
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
-        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+		testRhymers(factory);
 
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
-                rhymers[j].countIn(i);
+	}
 
-        java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+	private static void testRhymers(Rhymersfactory factory) {
+		DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
+				factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
-        for (int i = 0; i < rhymers.length; i++) {
-            while (!rhymers[i].callCheck())
-                System.out.print(rhymers[i].countOut() + "  ");
-            System.out.println();
-        }
+		for (int i = 1; i < 15; i++)
+			for (int j = 0; j < 3; j++)
+				rhymers[j].countIn(i);
 
-        System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+		java.util.Random rn = new java.util.Random();
+		for (int i = 1; i < 15; i++)
+			rhymers[3].countIn(rn.nextInt(20));
 
-    }
+		for (int i = 0; i < rhymers.length; i++) {
+			while (!rhymers[i].callCheck())
+				System.out.print(rhymers[i].countOut() + "  ");
+			System.out.println();
+		}
+
+		System.out.println("total rejected is "
+				+ ((HanoiRhymer) rhymers[3]).reportRejected());
+	}
 
 }
